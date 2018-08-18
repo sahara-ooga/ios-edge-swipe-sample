463496685b0c67430530ead1b67122a1af2df41e
yuu ogasawara <performa532@gmail.com>
2018-08-18 09:50:05 +0900

    Initial Commit

---
+++ slide-in-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slide-in-sample
+//
+//  Created by ogasawara_dev on 2018/08/18.
+//  Copyright © 2018年 ogasawara_dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
